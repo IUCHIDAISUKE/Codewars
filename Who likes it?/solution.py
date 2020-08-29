f49f4220d90a4a3cd0765d327638bb4d79068727
Iuchi Daisuke <33daisuke07@gmail.com>
2020-08-29 16:59:10 +0900

    Bit counting, Who likes it?

---
+++ Who likes it?/solution.py
@@ -0,0 +1,12 @@
+def likes(names):
+    n = len(names)
+    if not n:
+        return 'no one likes this'
+    elif n == 1:
+        return names[0]+" likes this"
+    elif n == 2:
+        return names[0] + " and " + names[1] + " like this"
+    elif n == 3:
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this"
+    else:
+        return names[0] + ", " + names[1] + " and " + str(n-2) + " others like this"
