01c0d2fa8e947683098cb5198c9ad95939a1ac11
Iuchi Daisuke <33daisuke07@gmail.com>
2020-09-03 19:39:42 +0900

    modified gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-def add(num1, num2):
-    res = ""
-    while num1 > 0 or num2 > 0:
-        res = str(num1 % 10 + num2 % 10) + res
-        num1 //= 10
-        num2 //= 10
-    return int(res) if res else 0
