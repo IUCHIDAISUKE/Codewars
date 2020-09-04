15d73faa193711229361fdc14d7903bb4ab1787d
Iuchi Daisuke <33daisuke07@gmail.com>
2020-09-04 19:02:54 +0900

    modified gitignore(add tempCodeRunnerFile)

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-find_nth_occurrence(substring, string, occurrence=1):
-    idx = -1
-    for i in range(occurrence):
-        idx = string.find(substring, idx + 1)
-        if idx == -1:
-            return -1
-    return idx
